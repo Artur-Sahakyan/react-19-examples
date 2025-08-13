5266ccdcee41c641d95afc0f9272fe0f62e663fc
Artur-Sahakyan <archer@logixsoftware.com>
2025-08-13 16:23:59 +0400

    React (v19) new hooks examples

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { Suspense } from "react";
+import Users from "./examples/Users";
+import Theme from "./examples/Theme";
+import Optimistic from "./examples/Optimistic";
+import FormStatus from "./examples/FormStatus";
+import FormState from "./examples/FormState";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+
+function App() {
+  const fetchPostsPromise = fetchPosts();
+  return (
+    <>
+      <div>
+        <Suspense
+          fallback={ <h1 className="text-2xl text-center font-bold mt-5">Loading...</h1> }
+        >
+          <Users fetchPostsPromise={fetchPostsPromise} />
+          <FormState />
+          <FormStatus />
+          <Optimistic />
+          <Theme />
+
+        </Suspense>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/examples/Users.jsx
@@ -0,0 +1,23 @@
+import { use } from "react";
+
+const PostItems = ({ fetchPostsPromise }) => {
+  const users = use(fetchPostsPromise);
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <div key={user.id} className="bg-blue-900 shadow-md p-4 my-6 rounded-lg">
+          <h2 className="text-xl font-bold">{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </ul>
+  );
+};
+import PropTypes from "prop-types";
+
+PostItems.propTypes = {
+  fetchPostsPromise: PropTypes.any.isRequired,
+};
+
+export default PostItems;
