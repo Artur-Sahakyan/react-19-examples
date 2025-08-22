dd72d554741757f260253aa01cdc4da992fa2a0a
Artur-Sahakyan <archer@logixsoftware.com>
2025-08-22 13:37:44 +0400

    use useFormState hook

--- src/examples/FormState.jsx
+++ src/examples/FormState.jsx
@@ -1,33 +1,41 @@
-import { useFormState} from 'react-dom';
+"use client";
+
+import { useFormState } from "react-dom";
+
+const handleSubmit = async (previousState, formData) => {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const res =  await fetch("https://jsonplaceholder.typicode.com/users").then(r => r.json());
+
+  const data = res.slice(0,1);
+
+  return { name, email, status: "submitted", data };
+};
 
 const FormState = () => {
-    const submitForm = (prevState, queryData) => {
-        const name =  queryData.get("username");
-        if(name === 'Artur'){
-            return {
-                success: true,
-                text: "Welcome"
-            }
-        }
-        else{
-            return {
-                success: false,
-                text: "ERRORRR"
-            }
-        }
-    }
-    const [ message, formAction ] = useFormState(submitForm, null)
 
-    return <>
-    <h3>useFormState() example</h3>
-    <form action={formAction} className='my-3'>
-        <label>Name</label>
-        <input type="text" name="username" className="border-2 rounded-md"/>
-        <br />
-        <button className="bg-blue-400">Submit</button>
-        {message && <h4>{message.text}</h4>}
-    </form>
+  const [formState, formAction] = useFormState(handleSubmit, {
+    name: "initial name",
+    email: "init email",
+  });
+
+  console.log(formState, ' formState')
+  return (
+    <>
+      <div>{JSON.stringify(formState.data)}</div>
+      <div>{formState.email}</div>
+      <div>{formState.name}</div>
+
+      <form action={formAction} className="space-x-2">
+        <input name="name" className="border-2 rounded-md p-1" />
+        <input name="email" className="border-2 rounded-md p-1" />
+
+        <button type="submit" className="bg-blue-400 px-4 py-1 rounded-md">
+          Submit
+        </button>
+      </form>
     </>
-}
+  );
+};
 
 export default FormState;
